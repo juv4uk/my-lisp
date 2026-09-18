c9168d51312dc6853ce870b27a94df0ad10fdd03
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-09-18 21:28:27 +0300

    fix(#658): balance final UTF-8 encode cond

--- lib/utf8.lisp
+++ lib/utf8.lisp
@@ -285,7 +285,7 @@
                           (cons (+ 128 (mod (quotient scalar 64) 64))
                                 (cons (+ 128 (mod (quotient scalar 4096) 64))
                                       (cons (+ 240 (quotient scalar 262144))
-                                            out))))))))))))))))
+                                            out)))))))))))))))
 
 (def utf8-encode-string
   (lambda (text)
