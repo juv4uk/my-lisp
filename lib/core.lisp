af3f0c497a73959932f53017dde45b9d1af723ab
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-09-18 21:08:22 +0300

    feat(#352): derive Lisp-owned explicit text ordering

--- lib/core.lisp
+++ lib/core.lisp
@@ -439,6 +439,30 @@
        (identity-relation same)
        (quote (text-order after))))))
 
+(def string-order
+  (lambda (left right)
+    (cond
+      ((string-empty? left) (identity-relation same)
+       (cond
+         ((string-empty? right) (identity-relation same)
+          (quote (text-order same)))
+         ((string-empty? right) (identity-relation distinct)
+          (quote (text-order before)))))
+      ((string-empty? left) (identity-relation distinct)
+       (quote (text-order after)))
+      ((eq (string-first left) (string-first right)) (identity-relation same)
+       (string-order (string-rest left) (string-rest right)))
+      ((eq (< (string->codepoint (string-first left))
+              (string->codepoint (string-first right)))
+           1)
+       (identity-relation same)
+       (quote (text-order before)))
+      ((eq (< (string->codepoint (string-first left))
+              (string->codepoint (string-first right)))
+           0)
+       (identity-relation same)
+       (quote (text-order after))))))
+
 (def string-length
   (lambda (s)
     (cond
